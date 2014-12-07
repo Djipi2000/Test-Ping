f4150752fec11813dff8f962353d10812c2f13ca
Djipi <ghislain.piot@free.fr>
2014-12-07 20:47:26 +0100

    V2

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -29,8 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,6 +37,10 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timernotifyicon = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,11 +67,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(208, 12);
+            this.label1.Location = new System.Drawing.Point(307, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ping";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
@@ -76,10 +80,8 @@
             // 
             // chart1
             // 
-            chartArea5.Name = "PingArea";
-            this.chart1.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chart1.Legends.Add(legend5);
+            chartArea2.Name = "PingArea";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Location = new System.Drawing.Point(12, 41);
             this.chart1.Name = "chart1";
             this.chart1.Size = new System.Drawing.Size(659, 261);
@@ -89,7 +91,7 @@
             // 
             // timer2
             // 
-            this.timer2.Interval = 1000;
+            this.timer2.Interval = 980;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // timer3
@@ -98,11 +100,44 @@
             this.timer3.Interval = 60000;
             this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(188, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "RaZ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ping";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // timernotifyicon
+            // 
+            this.timernotifyicon.Enabled = true;
+            this.timernotifyicon.Interval = 3000;
+            this.timernotifyicon.Tick += new System.EventHandler(this.timernotifyicon_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 352);
+            this.ClientSize = new System.Drawing.Size(677, 312);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -124,6 +159,10 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timernotifyicon;
     }
 }
 
--- Test/Form1.cs
+++ Test/Form1.cs
@@ -17,18 +17,35 @@ namespace Test
         Ping ping_euw = new Ping();
         Series serie1 = new Series();
         long ping_every5;
+        int compteur;
         
         public Form1()
         {
             InitializeComponent();
   //          Ping ping_euw = new Ping();  3
             chart1.Series.Add(serie1);
-
+            compteur = 0;
+            notifyIcon1.Text = "Lol Verificateur de Ping";
+            notifyIcon1.Icon = new Icon("appicon.ico");
+            notifyIcon1.Visible = true;
+            notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
         }
+        private void notifyIcon1_DoubleClick(object Sender, EventArgs e)
+        {
+            // Show the form when the user double clicks on the notify icon.
+
+            // Set the WindowState to normal if the form is minimized.
+            if (this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+
+            // Activate the form.
+            this.Activate();
+        }
+
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            PingReply pingReply = ping_euw.Send("google.fr");
+            PingReply pingReply = ping_euw.Send("tavernaute.net");
             label1.Text = (pingReply.RoundtripTime.ToString() + "ms");
             ping_every5 = pingReply.RoundtripTime;
 
@@ -39,23 +56,19 @@ namespace Test
         {
             timer1.Enabled = true;
             timer2.Enabled = true;
+            timer3.Enabled = true;
+            timernotifyicon.Enabled = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
             timer2.Enabled = false;
+            timer3.Enabled = false;
+            timernotifyicon.Enabled = false;
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
-        }
-
-        private void chart1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void chart1_Click_1(object sender, EventArgs e)
         {
@@ -64,13 +77,52 @@ namespace Test
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            serie1.Points.Add(ping_every5);
+            if (compteur < 10)
+            {
+                serie1.Points.Add(ping_every5);
+            }
+            else
+            {
+                serie1.Points.Clear();
+                serie1.Points.Add(ping_every5);
+                compteur = 0;
+            }
+            compteur = compteur + 1;
+
+
 
         }
 
         private void timer3_Tick(object sender, EventArgs e)
         {
             serie1.Points.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serie1.Points.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timernotifyicon_Tick(object sender, EventArgs e)
+        {
+            if (ping_every5 >= 100)
+            {
+                notifyIcon1.Icon = SystemIcons.Exclamation;
+                notifyIcon1.BalloonTipTitle = "Lol Ping";
+                notifyIcon1.BalloonTipText = ("Votre ping est trop haut (" + ping_every5 + "ms)");
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Warning;
+                notifyIcon1.ShowBalloonTip(60);
+            }
         }
     }
 }
